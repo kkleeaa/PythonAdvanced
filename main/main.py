7e7e6d4beb5f0c4785b24c159772341f9c41ec17
Studenti <rexhepiklea123@gmail.com>
2024-10-18 17:00:40 +0200

    added variables list

---
+++ main/main.py
@@ -0,0 +1,14 @@
+emri='klea'
+torta='tiramisu'
+mosha=17
+type(emri)
+print(type(emri))
+
+mosha = mosha + 10
+print(mosha)
+emri_mbiemri = emri + ' Rexhepi'
+my_data= [ 'klea' , 'Rexhepi' , '17' ]
+print (my_data)
+my_data.append ('Prishtina')
+print(my_data)
+my_data.insert(_index:4 , _object
